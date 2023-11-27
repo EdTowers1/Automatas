c63ecdcfe120925685ac46478ba863b459a730aa
programador7 <programador7@tecnologicocomfenalco.edu.co>
2023-11-27 16:09:21 -0500

    Validaciones y mensajes funcionales

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-
-
-
-
 const regexToBeAfirmativo =
-  /^((I('m| am)|You('re| are)|He('s| is)|She('s| is)|It('s| is)|We('re| are)|They('re| are))|[A-Z][a-z]*('s| is)|The .+ (is|'s)) .+$/;
+  /^(I('m| am)|You('re| are)|He('s| is)|She('s| is)|It('s| is)|We('re| are)|They('re| are)|[A-Z][a-z]*('s| is)|[A-Z][a-z]* .+|The [A-Z][a-z]+ (am|is|are)) .+$/;
 
-  function validarToBeAfirmativo(cadena) {
+function validarToBeAfirmativo(cadena) {
   return regexToBeAfirmativo.test(cadena);
 }
 
@@ -19,11 +15,19 @@ function validarOraciones() {
   // Obtener las oraciones del textarea
   const oraciones = inputOraciones.value.split("\n");
 
+  // Inicializar el contador de oraciones correctas
+  let correctCount = 0;
+
   // Validar cada oración y mostrar el resultado
   oraciones.forEach((oracion, index) => {
     const esValida = validarToBeAfirmativo(oracion.trim());
     const resultado = esValida ? "Correcta" : "Incorrecta";
     const color = esValida ? "green" : "red";
+
+    // Incrementar el contador si la oración es correcta
+    if (esValida) {
+      correctCount++;
+    }
 
     // Crear un elemento div para mostrar el resultado
     const resultadoElement = document.createElement("div");
@@ -33,6 +37,22 @@ function validarOraciones() {
     // Agregar el resultado al contenedor
     resultadoDiv.appendChild(resultadoElement);
   });
+
+  // Agregar un pequeño retraso antes de mostrar la alerta
+  setTimeout(() => {
+    // Determinar el mensaje final según el contador
+    let message = "";
+    if (correctCount === oraciones.length) {
+      message = "Excelente, todas fueron correctas!";
+    } else if (correctCount >= oraciones.length / 2) {
+      message = "Muy bien, pero puedes mejorar";
+    } else {
+      message = "Tienes que estudiar más";
+    }
+
+    // Mostrar el mensaje final
+    alert(message);
+  }, 100);
 }
 
 const inputOraciones = document.getElementById("oraciones");
